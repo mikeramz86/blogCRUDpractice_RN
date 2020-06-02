2e6dcb032dc555dd152971f30a490f09ce159fe1
Mike <mikeramzdesign@gmail.com>
2020-06-02 12:52:15 -0700

    added prettier

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,26 +1,27 @@
-import React, {useState, useContext} from 'react'
-import {View, Text, StyleSheet, TextInput} from 'react-native'
-import {Context} from '../context/BlogContext'
+import React, { useState, useContext } from 'react'
+import { View, Text, StyleSheet, TextInput } from 'react-native'
+import { Context } from '../context/BlogContext'
 
-const EditScreen = ({navigation}) => {
-    const {state} = useContext(Context)
+const EditScreen = ({ navigation }) => {
+  const { state } = useContext(Context)
 
-    const blogPost = state.find(
-        blogPost => blogPost.id === navigation.getParam('id')
-    )
-    
-    const [title, setTitle] = useState(blogPost.title)
+  const blogPost = state.find(
+    (blogPost) => blogPost.id === navigation.getParam('id')
+  )
 
-    const [content, setContent] = useState(blogPost.content)
-    
-    
-    
-    return (
-        <View>
-            <Text> Edit Title: </Text>
-            <TextInput value={title} onChangeText={(newTitle) => setTitle(newTitle)} />
-        </View>
-    )
+  const [title, setTitle] = useState(blogPost.title)
+
+  const [content, setContent] = useState(blogPost.content)
+
+  return (
+    <View>
+      <Text> Edit Title: </Text>
+      <TextInput
+        value={title}
+        onChangeText={(newTitle) => setTitle(newTitle)}
+      />
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({})
--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -4,29 +4,31 @@ import { Context } from '../context/BlogContext'
 import { EvilIcons } from '@expo/vector-icons'
 
 const ShowScreen = ({ navigation }) => {
-    const { state } = useContext(Context)
-    const blogPost = state.find((blogPost) => blogPost.id === navigation.getParam('id'))
+  const { state } = useContext(Context)
+  const blogPost = state.find(
+    (blogPost) => blogPost.id === navigation.getParam('id')
+  )
 
-    return (
-        <View>
-            <Text> {blogPost.title}</Text>
-            <Text> {blogPost.content}</Text>
-        </View>
-    )
+  return (
+    <View>
+      <Text> {blogPost.title}</Text>
+      <Text> {blogPost.content}</Text>
+    </View>
+  )
 }
 
-ShowScreen.navigationOptions = ({navigation}) => {
-    return {
-        headerRight: (       
-            <TouchableOpacity 
-                onPress={() => 
-                    navigation.navigate('Edit', {id: navigation.getParam('id')})
-                }
-            >
-                    <EvilIcons name="pencil" size={35} />
-            </TouchableOpacity>
-        )
-    }
+ShowScreen.navigationOptions = ({ navigation }) => {
+  return {
+    headerRight: (
+      <TouchableOpacity
+        onPress={() =>
+          navigation.navigate('Edit', { id: navigation.getParam('id') })
+        }
+      >
+        <EvilIcons name='pencil' size={35} />
+      </TouchableOpacity>
+    ),
+  }
 }
 
 const styles = StyleSheet.create({})
